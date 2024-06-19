e8bb99dcc2792b4e3c415435466ef84283f8ed5b
Mareks <cyonpro47@gmail.com>
2024-06-20 01:24:09 +0300

    First

---
+++ Uwork_Latest_1/Uwork_try_SeeMore/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div id="app" class="bg-white md:w-8/12 m-auto rounded-xl shadow-xl md:mt-14 mt-0 w-full p-4">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<style scoped></style>
